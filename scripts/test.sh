56456ea6f8618f61f8f3d1a814cdc7ab03be0f2e
Joshua Lan <josh.lan9@gmail.com>
2018-02-13 17:46:12 -0500

    Delete test.sh

--- scripts/test.sh
+++
@@ -1,1 +0,0 @@
-curl -G --user "b8c9ab8b-278e-49c9-bc41-309ce83a7612":"RdCKtar5NO7O" "htps://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/33c2fbx273-nlc-24419/classify" --data-urlencode "text=I hate swimming"
